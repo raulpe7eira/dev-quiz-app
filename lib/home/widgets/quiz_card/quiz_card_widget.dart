42b2db8cd8fbc4c7156c317b37d1685046f049e2
Raul Pereira <raul.pereira@gmail.com>
2021-04-20 20:37:23 -0300

    add challenge page

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:dev_quiz/core/app_colors.dart';
 import 'package:dev_quiz/core/app_images.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/shared/widgets/progress_indicator/progess_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -37,13 +38,7 @@ class QuizCardWidget extends StatelessWidget {
               ),
               Expanded(
                 flex: 4,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor: AlwaysStoppedAnimation<Color>(
-                    AppColors.chartPrimary,
-                  ),
-                ),
+                child: ProgressIndicatorWidget(value: 0.3),
               )
             ],
           )
