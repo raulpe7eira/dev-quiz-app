35efe4153270e9a2d651e56660eb0026ef605b09
Raul Pereira <raul.pereira@gmail.com>
2021-04-22 18:08:22 -0300

    add next button

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,5 +1,7 @@
+import 'package:dev_quiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
+
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
@@ -16,6 +18,30 @@ class _ChallengePageState extends State<ChallengePage> {
         child: SafeArea(top: true, child: QuestionIndicatorWidget()),
       ),
       body: QuizWidget(title: "O que o Flutter faz em sua totalidade?"),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: NextButtonWidget.white(
+                  label: "Cancelar",
+                  onTap: () {},
+                ),
+              ),
+              SizedBox(width: 7),
+              Expanded(
+                child: NextButtonWidget.green(
+                  label: "Confirmar",
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -0,0 +1,60 @@
+import 'package:dev_quiz/core/app_colors.dart';
+
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class NextButtonWidget extends StatelessWidget {
+  final Color backgroundColor;
+  final Color borderColor;
+  final Color fontColor;
+  final String label;
+  final VoidCallback onTap;
+
+  NextButtonWidget({
+    required this.backgroundColor,
+    required this.borderColor,
+    required this.fontColor,
+    required this.label,
+    required this.onTap,
+  });
+
+  NextButtonWidget.green({required this.label, required this.onTap})
+      : this.backgroundColor = AppColors.darkGreen,
+        this.borderColor = AppColors.green,
+        this.fontColor = AppColors.white;
+
+  NextButtonWidget.white({required this.label, required this.onTap})
+      : this.backgroundColor = AppColors.white,
+        this.borderColor = AppColors.border,
+        this.fontColor = AppColors.grey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            backgroundColor,
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+          side: MaterialStateProperty.all(
+            BorderSide(color: borderColor),
+          ),
+        ),
+        onPressed: onTap,
+        child: Text(
+          label,
+          style: GoogleFonts.notoSans(
+            fontWeight: FontWeight.w600,
+            fontSize: 15,
+            color: fontColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
