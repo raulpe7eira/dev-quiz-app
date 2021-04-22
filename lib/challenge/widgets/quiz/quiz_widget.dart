c48043a609569b9e37106d019202cc4fba546493
Raul Pereira <raul.pereira@gmail.com>
2021-04-22 20:39:41 -0300

    add quiz controller

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -4,13 +4,22 @@ import 'package:dev_quiz/shared/models/question_model.dart';
 
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
+  final VoidCallback onChange;
 
   const QuizWidget({
     Key? key,
     required this.question,
+    required this.onChange,
   }) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +27,20 @@ class QuizWidget extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: 64),
-          Text(question.title, style: AppTextStyles.heading),
+          Text(widget.question.title, style: AppTextStyles.heading),
           SizedBox(height: 24),
-          ...question.answers.map(
-            (e) => AnswerWidget(title: e.title, isRight: e.isRight),
-          ),
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              answer: widget.question.answers[i],
+              isSelected: indexSelected == i,
+              disabled: indexSelected != -1,
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+                Future.delayed(Duration(seconds: 1))
+                    .then((value) => widget.onChange());
+              },
+            ),
         ],
       ),
     );
